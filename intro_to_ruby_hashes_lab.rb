4ee58f7b9f0fb2c95433e8606823a6d777ef5114
Omar Nabi <onabi20@gmail.com>
2020-04-03 05:27:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ end
 
 def monopoly_with_fourth_tier
 	monopoly = {:railroads => {
-end	    :pieces => 4,
+	  :pieces => 4,
 
     :names => {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline => {"mortgage_value" => "$800"} },
 
