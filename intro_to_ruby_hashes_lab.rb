400ba2feda69bbfdf073301c56ee8eea5c8137d7
Omar Nabi <onabi20@gmail.com>
2020-04-03 05:19:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,13 +12,15 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	 monopoly = {:railroads => {
+	monopoly = {
 
-    :pieces => 4,
-    :names => {},
-    :rent_in_dollars => {}
-  } 
-  }
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
+	}
 
 end
 
